762d57ba158d97f4ce21502c76cefd76f37e221c
rhaeckl <rm.haeckl@gmail.com>
2020-03-20 22:30:59 +0100

    initial commit

---
+++ src/main/java/com/reserveme/ReservemeApplication.java
@@ -0,0 +1,13 @@
+package com.reserveme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reserveme/reserveme/ReservemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserveme.reserveme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
