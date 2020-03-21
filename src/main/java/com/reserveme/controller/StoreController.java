24878baa4a985c725b7c9b0999963d0b84a90bb4
Tim Rader <tim.rader@otto.de>
2020-03-21 10:22:41 +0100

    Fix request mapping and add logging

--- src/main/java/com/reserveme/controller/StoreController.java
+++ src/main/java/com/reserveme/controller/StoreController.java
@@ -3,10 +3,15 @@ package com.reserveme.controller;
 import com.reserveme.model.Store;
 import com.reserveme.response.StoreAddResponse;
 import com.reserveme.service.StoreService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(StoreController.ENDPOINT)
+@Slf4j
+@RestController
+@RequestMapping(StoreController.ENDPOINT)
 public class StoreController {
 
     static final String ENDPOINT = "/store";
@@ -18,7 +23,8 @@ public class StoreController {
     }
 
     @PostMapping
-    public StoreAddResponse addStore(Store store) {
+    public StoreAddResponse addStore(@RequestBody Store store) {
+        log.info("Received request to add store {}", store);
         return new StoreAddResponse(storeService.addStore(store));
     }
 }
